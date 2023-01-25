ce799ff63d4f8092027e666315a063fd6a295826
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-25 15:46:39 -0300

    create notes css

--- src/pages/CreateNotes.jsx
+++ src/pages/CreateNotes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineArrowLeft as Arrow } from 'react-icons/ai'
+import { BsSave as Save } from 'react-icons/bs'
 
 const CreateNotes = () => {
   return (
     <section>
       <header className="create-notes__header">
         <Link to='/notes-react' className='btn btn-large'><Arrow /></Link>
-        <button className='btn btn-large'>Salvar</button>
+        <button className='btn btn-large'>Salvar <Save /></button>
       </header>
       <form className='create-notes__form'>
         <input type="text" autoFocus placeholder='Título...' />
