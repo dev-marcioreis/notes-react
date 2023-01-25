87b4b08ecfdfdd61d756448593c12429fe8416a2
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-25 15:30:58 -0300

    iniciando create notes jsx

--- src/pages/CreateNotes.jsx
+++ src/pages/CreateNotes.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { AiOutlineArrowLeft as Arrow } from 'react-icons/ai'
 
 const CreateNotes = () => {
   return (
-    <div>CreateNotes</div>
+    <section>
+      <header className="create-notes__header">
+        <Link to='/notes-react' className='btn btn-large'><Arrow /></Link>
+        <button className='btn btn-large'>Salvar</button>
+      </header>
+      <form className='create-notes__form'>
+        <input type="text" autoFocus placeholder='Título...' />
+        <textarea rows="28" placeholder='Adicionar informação...'></textarea>
+      </form>
+    </section>
   )
 }
 
